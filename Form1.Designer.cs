5208213e0521fb274f5cea7790ae6ed17830984b
Retra <50384498+RetraA@users.noreply.github.com>
2021-08-17 18:45:06 -0400

    Add files via upload
    inital

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace BurgerShot_example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.costLabel = new System.Windows.Forms.Label();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(319, 370);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Heartstopper";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "MeatFree";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(345, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 64);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Soft drink";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(661, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 64);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Bleeder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(506, 111);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 64);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Fries";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(661, 111);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(127, 64);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Moneyshot";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(661, 41);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(127, 64);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Torpedo";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(345, 181);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(127, 64);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Milkshake";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(506, 41);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(127, 64);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Donut";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(502, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Single Items";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // costLabel
+            // 
+            this.costLabel.AutoSize = true;
+            this.costLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.costLabel.Location = new System.Drawing.Point(341, 327);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(79, 20);
+            this.costLabel.TabIndex = 16;
+            this.costLabel.Text = "Total: $0";
+            this.costLabel.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(345, 350);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(91, 33);
+            this.button13.TabIndex = 17;
+            this.button13.Text = "Void All";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(345, 111);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(127, 64);
+            this.button14.TabIndex = 18;
+            this.button14.Text = "Water";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(345, 41);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(127, 64);
+            this.button15.TabIndex = 19;
+            this.button15.Text = "Coffee";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(661, 251);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(127, 64);
+            this.button16.TabIndex = 20;
+            this.button16.Text = "NOTHING";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 388);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Burger Shot Cash Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BurgerShot_example
+{
+    public partial class Form1 : Form
+    {
+        int totalCost = 0;
+        int heartStopper = 800;
+        int bleeder = 500;
+        int fries = 200;
+        int torpedo = 600;
+        int moneyShot = 400;
+        int meatFree = 1000;
+        int donut = 150;
+        int coffee = 750;
+        int water = 200;
+        int softDrink = 400;
+        int milkShake = 750;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            totalCost = heartStopper + totalCost;
+            textBox1.AppendText("Heart Stopper - $800\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $"+totalCost.ToString();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            totalCost = bleeder + totalCost;
+            textBox1.AppendText("Bleeder - $500\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            totalCost = fries + totalCost;
+            textBox1.AppendText("Fries - $200\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            totalCost = 0;
+            costLabel.Text = "Total: $"+totalCost.ToString();
+            textBox1.Clear();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            totalCost = torpedo + totalCost;
+            textBox1.AppendText("Torpedo - $600\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            totalCost = moneyShot + totalCost;
+            textBox1.AppendText("Money Shot - $400\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            totalCost = meatFree + totalCost;
+            textBox1.AppendText("Meat Free - $1000\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            totalCost = donut + totalCost;
+            textBox1.AppendText("Donut - $500\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            totalCost = milkShake + totalCost;
+            textBox1.AppendText("Milkshake - $750\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            totalCost = softDrink + totalCost;
+            textBox1.AppendText("Soft Drink - $400\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            totalCost = water + totalCost;
+            textBox1.AppendText("Water - $200\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+
+        private void Button15_Click(object sender, EventArgs e)
+        {
+            totalCost = coffee + totalCost;
+            textBox1.AppendText("Coffee - $750\n");
+            textBox1.AppendText(Environment.NewLine);
+            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
+        }
+    }
+}
