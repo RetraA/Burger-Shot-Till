29b8e79e8f317630e1bc0ecb3b4038bee11be04a
Retra <50384498+RetraA@users.noreply.github.com>
2022-02-18 12:28:49 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -13,16 +13,15 @@ namespace BurgerShot_example
     public partial class Form1 : Form
     {
         int totalCost = 0;
-        int heartStopper = 800;
-        int bleeder = 500;
-        int fries = 200;
-        int moneyShot = 600;
-        int meatFree = 1000;
-        int donut = 150;
-        int meteoriteIceCream = 750;
-        int water = 200;
-        int softDrink = 400;
-        int otIceCream = 750;
+        int heartStopper = 400;
+        int bleeder = 150;
+        int fries = 50;
+        int moneyShot = 200;
+        int meatFree = 150;
+        int donut = 50;
+        int meteoriteIceCream = 150;
+        int softDrink = 100;
+        int otIceCream = 150;
 
 
 
@@ -39,7 +38,7 @@ namespace BurgerShot_example
         private void Button2_Click(object sender, EventArgs e)
         {
             totalCost = heartStopper + totalCost;
-            textBox1.AppendText("Heart Stopper - $800\n");
+            textBox1.AppendText("Heart Stopper - $400\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $"+totalCost.ToString();
         }
@@ -47,7 +46,7 @@ namespace BurgerShot_example
         private void Button4_Click(object sender, EventArgs e)
         {
             totalCost = bleeder + totalCost;
-            textBox1.AppendText("Bleeder - $500\n");
+            textBox1.AppendText("Bleeder - $150\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
@@ -60,7 +59,7 @@ namespace BurgerShot_example
         private void Button5_Click(object sender, EventArgs e)
         {
             totalCost = fries + totalCost;
-            textBox1.AppendText("Fries - $200\n");
+            textBox1.AppendText("Fries - $50\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
@@ -85,7 +84,7 @@ namespace BurgerShot_example
         private void Button6_Click(object sender, EventArgs e)
         {
             totalCost = moneyShot + totalCost;
-            textBox1.AppendText("Money Shot - $600\n");
+            textBox1.AppendText("Money Shot - $200\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
@@ -93,7 +92,7 @@ namespace BurgerShot_example
         private void Button1_Click(object sender, EventArgs e)
         {
             totalCost = meatFree + totalCost;
-            textBox1.AppendText("Meat Free - $1000\n");
+            textBox1.AppendText("Meat Free - $150\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
@@ -101,7 +100,7 @@ namespace BurgerShot_example
         private void Button9_Click(object sender, EventArgs e)
         {
             totalCost = donut + totalCost;
-            textBox1.AppendText("Donut - $150\n");
+            textBox1.AppendText("Donut - $50\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
@@ -109,7 +108,7 @@ namespace BurgerShot_example
         private void Button8_Click(object sender, EventArgs e)
         {
             totalCost = otIceCream + totalCost;
-            textBox1.AppendText("Orange Tango Ice Cream - $750\n");
+            textBox1.AppendText("Orange Tango Ice Cream - $150\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
@@ -117,7 +116,7 @@ namespace BurgerShot_example
         private void Button3_Click(object sender, EventArgs e)
         {
             totalCost = softDrink + totalCost;
-            textBox1.AppendText("Soft Drink - $400\n");
+            textBox1.AppendText("Soft Drink - $100\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
@@ -129,16 +128,12 @@ namespace BurgerShot_example
 
         private void Button14_Click(object sender, EventArgs e)
         {
-            totalCost = water + totalCost;
-            textBox1.AppendText("Water - $200\n");
-            textBox1.AppendText(Environment.NewLine);
-            costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
 
         private void Button15_Click(object sender, EventArgs e)
         {
             totalCost = meteoriteIceCream + totalCost;
-            textBox1.AppendText("Meteorite Ice Cream - $750\n");
+            textBox1.AppendText("Meteorite Ice Cream - $150\n");
             textBox1.AppendText(Environment.NewLine);
             costLabel.Text = "Total: $" + totalCost.ToString(); costLabel.Text = "Total: $" + totalCost.ToString();
         }
